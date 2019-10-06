778c5489713d9dc46beebd401d2654350ff558bc
Bartosz Kreft <bartosz.kreft.biz@gmail.com>
2019-10-06 16:57:35 +0200

    Add new folder Number Theory; Add a file GCD

---
+++ Number Theory/GCD.py
@@ -0,0 +1,39 @@
+# Implementation of a recursive Euclidean algorithm and an extended Euclidean algorithm
+def Euclidean(m,n):
+    m = int(m)
+    n = int(n)
+    if m%n == 0:
+        return n
+    else:
+        return Euclidean(n, m%n)
+
+def Extended_Euclidean(m,n):
+    m = int(m)
+    n = int(n)
+    x, px = 0, 1
+    y, py = 1, 0
+
+    while(n!=0):
+        c = m%n
+        d = int(m/n)
+        
+        m = n
+        n = c
+
+        temp = x
+        x = px - d*x
+        px = temp
+
+        temp = y
+        y = py - d*y
+        py = temp
+    # return m // this is the greatest common divisor of m ans n
+    return px, py
+
+a = input("Enter number a: ")
+b = input("Enter number b: ")
+
+x, y = Extended_Euclidean(a,b)
+
+print("GCD of numebers {0} and {1} is equal: {2}".format(a, b, Euclidean(a,b) ))
+print("Extended GCD: {0} = ({1}) * {2} + ({3}) * {4}  ".format(Euclidean(a,b), x, a, y, b))
